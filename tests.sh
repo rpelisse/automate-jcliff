d9a69fbaf64a8e3547019469b7c02c7017996c4a
Romain Pelisse <belaran@gmail.com>
2019-07-17 11:24:47 +0200

    tmp fix - to be removed of the history

--- tests.sh
+++ tests.sh
@@ -5,5 +5,11 @@ set -e
 export ANSIBLE_NOCOLOR='True'
 ansible-playbook -vvvv /work/wildfly-setup.yml
 ansible-galaxy install redhat-cop.jcliff
+# temporary fix
+readonly ROLE_DIR='/root/.ansible/roles/redhat-cop.jcliff/tasks'
+mkdir "${ROLE_DIR}"
+cp '/work/tasks/main.yml' "${ROLE_DIR}"
+# end of tmp fix
+
 ansible-playbook -vvvv /work/tune-wildfly-with-jcliff.yml --extra-vars "custom_rules_folder=/work/files/custom_rules"
 rm -rf /work/*
